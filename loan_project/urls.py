ee35cd7be8960f3bb400184a1a43e2d659dd523c
EsraaAt <esraa.arna1998@gmail.com>
2025-06-04 23:40:52 +0300

    Add URL routing for loans

--- loan_project/urls.py
+++ loan_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('loans/', include('loans.urls')),
+    path('', include('loans.urls')),  
 ]
