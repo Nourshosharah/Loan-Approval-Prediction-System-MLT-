4a1813827c72baece023567af8b377b7c1150c2d
madeha <madehayoussof96@gmail.com>
2025-06-05 13:28:03 +0300

    add App's providers and add basic style

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -1,5 +1,17 @@
-const App = () => {
-  return <div>App</div>;
-};
+import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import { router } from "./routes/AppRoutes";
+import { I18nProvider } from "./i18n";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <I18nProvider>
+        <RouterProvider router={router} />
+      </I18nProvider>
+    </Provider>
+  );
+}
 
 export default App;
