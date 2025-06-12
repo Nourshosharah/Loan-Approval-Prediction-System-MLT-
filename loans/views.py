d386b95cd5b22db1238dadcb64d738a7f0cbc8db
EsraaAt <esraa.arna1998@gmail.com>
2025-06-12 12:47:54 +0300

    Add basic home page with welcome message

--- loans/views.py
+++ loans/views.py
@@ -4,6 +4,12 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Loan
 from .serializers import LoanSerializer
+from django.http import HttpResponse
+
+
+
+def home(request):
+    return HttpResponse("<h2>Welcome to the Loan API </h2><p>Go to <a href='/api/loans/'>/api/loans/</a> to see all loans.</p>")
 
 @api_view(['GET', 'POST'])
 def loan_list(request):
@@ -18,3 +24,6 @@ def loan_list(request):
             serializer.save()  
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
