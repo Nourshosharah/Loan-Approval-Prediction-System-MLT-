a4cce08cf56286f5949d64b1ca84f1747a813769
EsraaAt <esraa.arna1998@gmail.com>
2025-06-20 16:41:38 +0300

    modify API links for uploading

--- Backend_part/loans/urls.py
+++ Backend_part/loans/urls.py
@@ -3,16 +3,16 @@ from .views import home, loan_list, loan_detail,loan_summary,predict_loan
 
 
 
+
 urlpatterns = [
     path('', home, name='home'), 
-    path('api/loans/', loan_list, name='loan_list'),
-    path('api/loans/<int:id>/', loan_detail, name='loan_detail'), 
-    path('api/loans/summary/', loan_summary, name='loan_summary'),
-    path('api/predict/', predict_loan, name='predict_loan'),
-
-
-
+    path('loans/', loan_list, name='loan_list'),
+    path('loans/<int:id>/', loan_detail, name='loan_detail'), 
+    path('loans/summary/', loan_summary, name='loan_summary'),
+    path('predict/', predict_loan, name='predict_loan'),
 ]
+
+
 
   
 
