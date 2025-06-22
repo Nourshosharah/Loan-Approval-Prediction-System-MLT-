b3b4470a81a7c88ebf74d603e0a50c1727f4d0c1
EsraaAt <esraa.arna1998@gmail.com>
2025-06-22 15:30:21 +0300

    Add loan status prediction endpoint with preprocessing and model inference

--- Backend_part/loans/urls.py
+++ Backend_part/loans/urls.py
@@ -1,17 +1,18 @@
 from django.urls import path
-from .views import home, loan_list, loan_detail,loan_summary,predict_loan
+from .views import home, loan_list, loan_detail,loan_summary,predict_loan_status
+
 
 
 
 
 urlpatterns = [
     path('', home, name='home'), 
-    path('loans/', loan_list, name='loan_list'),
-    path('loans/<int:id>/', loan_detail, name='loan_detail'), 
-    path('loans/summary/', loan_summary, name='loan_summary'),
-    path('predict/', predict_loan, name='predict_loan'),
-]
+    path('api/loans/', loan_list, name='loan_list'),
+    path('api/loans/<int:id>/', loan_detail, name='loan_detail'), 
+    path('api/loans/summary/', loan_summary, name='loan_summary'),
+   path('api/predict/', predict_loan_status, name='predict_loan_status'),
 
+]
 
 
   
--- Backend_part/loans/views.py
+++ Backend_part/loans/views.py
@@ -4,17 +4,19 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Loan
 from .serializers import LoanSerializer
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from django.shortcuts import get_object_or_404
 from django.db.models import Avg
 import joblib
 import numpy as np
-import os
+import pandas as pd
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 
 
 def home(request):
-    return HttpResponse("<h2>Welcome to the Loan API </h2><p>Go to <a href='/api/loans/'>/api/loans/</a> to see all loans.</p>")
+    return HttpResponse("<h2>Welcome to the Loan API</h2>")
 
 @api_view(['GET', 'POST'])
 def loan_list(request):
@@ -66,54 +68,85 @@ def loan_summary(request):
     })
 
 
-model_path = os.path.join(os.path.dirname(__file__), 'DecisionTree_mode.pkl')
-model = joblib.load(model_path)
 
-@api_view(['POST'])
-def predict_loan(request):
-    try:
-        data = request.data
+def preprocess_cols(df, features, caps):
+    df = df.copy()
+    object_cols = df.select_dtypes(include="object").columns.tolist()
+    if "Loan_ID" in object_cols:
+        object_cols.remove("Loan_ID")
+    if "Loan_Status" in object_cols:
+        object_cols.remove("Loan_Status")
 
-        total_income = float(data['applicant_income']) + float(data['coapplicant_income'])
-        loan_amount = float(data['loan_amount'])
-        loan_term_months = float(data['loan_amount_term'])
-        monthly_payment = loan_amount / loan_term_months if loan_term_months else 0
+    df["ApplicantIncome_log"] = np.log(df["ApplicantIncome"])
+    df["LoanAmount_log"] = np.log(df["LoanAmount"])
+    df["CoapplicantIncome_log"] = np.log1p(df["CoapplicantIncome"])
 
-        gender_male = 1 if data['gender'].lower() == 'male' else 0
-        married_yes = 1 if data['married'].lower() == 'yes' else 0
+    for col in ["ApplicantIncome_log", "LoanAmount_log", "CoapplicantIncome_log"]:
+        if caps is not None:
+            lower, upper = caps[col]
+        else:
+            lower, upper = -np.inf, np.inf
+        df[col + "_updated"] = np.where(
+            df[col] < lower, lower, np.where(df[col] > upper, upper, df[col])
+        )
 
-        dependents_1 = 1 if data['dependents'] == '1' else 0
-        dependents_2 = 1 if data['dependents'] == '2' else 0
-        dependents_3plus = 1 if data['dependents'] in ['3', '3+'] else 0
+    df_final = pd.get_dummies(df, columns=object_cols)
+    df_final["Total_income"] = df_final["ApplicantIncome_log_updated"] + df_final["CoapplicantIncome_log_updated"]
+    df_final["month_payment"] = df_final["LoanAmount_log_updated"] / df_final["Loan_Amount_Term"]
+    df_final = df_final.reindex(columns=features, fill_value=0)
 
-        education_not_grad = 1 if data['education'].lower() == 'not graduate' else 0
-        self_employed_yes = 1 if data['self_employed'].lower() == 'yes' else 0
-
-        property_area_semiurban = 1 if data['property_area'].lower() == 'semiurban' else 0
-        property_area_urban = 1 if data['property_area'].lower() == 'urban' else 0
-  
-
-        credit_history = int(data['credit_history'])  
+    return df_final
 
 
-        features = np.array([[
-            credit_history,
-            gender_male,
-            married_yes,
-            dependents_1,
-            dependents_2,
-            dependents_3plus,
-            education_not_grad,
-            self_employed_yes,
-            property_area_semiurban,
-            property_area_urban,
-            total_income,
-            monthly_payment
-        ]])
+model_path = r'C:\Users\omar_oz\Desktop\MCS\F24\HW_F24_MLT\MLT_Project\Backend_part\loans\DecisionTree_mode.pkl'
+with open(model_path, 'rb') as f:
+    model = joblib.load(f)
+    features = [
+    "Credit_History",
+    "Gender_Male",
+    "Married_Yes",
+    "Dependents_1",
+    "Dependents_2",
+    "Dependents_3+",
+    "Education_Not Graduate",
+    "Self_Employed_Yes",
+    "Property_Area_Semiurban",
+    "Property_Area_Urban",
+    "Total_income",
+    "month_payment",
+]
 
-        prediction = model.predict(features)[0]
-        result = "Approved" if prediction == "Y" else "Rejected"
-        return Response({"prediction": result})
+caps = {
+    "ApplicantIncome_log": (6.914567376450192, 9.714858994637867),
+    "LoanAmount_log": (3.8625058531812333, 5.849577627901635),
+    "CoapplicantIncome_log": (-11.609845226339234, 19.349742043898722),
+}
 
-    except Exception as e:
-        return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+@csrf_exempt
+def predict_loan_status(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+
+            df_input = pd.DataFrame([data])
+            df_processed = preprocess_cols(df_input, features, caps)
+
+            prediction = model.predict(df_processed)[0]
+            proba = model.predict_proba(df_processed)[0].tolist()
+
+            result = {
+                "prediction": bool(prediction),
+                "probability": {"Rejected": proba[0], "Accepted": proba[1]},
+            }
+
+            return JsonResponse(result)
+
+        except Exception as error:
+            return JsonResponse({"error": str(error)}, status=400)
+
+    else:
+        return JsonResponse({"error": "Only POST requests ."}, status=405)
+
+
+
+
